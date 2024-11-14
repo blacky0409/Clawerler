7b32e918dce6a4cc3e6d0a6b93dfe4812d65d605
blacky0409 <soccer1356@ajou.ac.kr>
2024-11-14 18:04:56 +0900

    commit

--- mining.py
+++ mining.py
@@ -1,6 +1,8 @@
 #요리 이름, 재료, 레시피, 인분, 조리시간
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import csv
 import pandas as pd
 
@@ -9,68 +11,78 @@ baseUrl = 'https://www.10000recipe.com'
 def PageCrawler(Url):
 	url = baseUrl + Url
 
+	session = requests.Session()
+	retry = Retry(total=5,  # 최대 재시도 횟수
+		backoff_factor=1,  # 재시도 간 대기 시간 증가
+		status_forcelist=[500, 502, 503, 504])  # 재시도할 상태 코드
+	adapter = HTTPAdapter(max_retries=retry)
+	session.mount('http://', adapter)
+	session.mount('https://', adapter)
+
 	page = requests.get(url)
 	recipe_source = [] #재료
 	recipe_step = [] #레시피 순서
 	recipe_title = ""
 	recipe_num = ""
 	recipe_time = ""
-
-	if page.status_code == 200:
-		soup= BeautifulSoup(page.content, 'html.parser')
+	try:
+		if page.status_code == 200:
+			soup= BeautifulSoup(page.content, 'html.parser')
 	
-		#이름 recipe_title
-		#인분 recipe_num
-		#조리시간 recipe_time
+			#이름 recipe_title
+			#인분 recipe_num
+			#조리시간 recipe_time
 
-		try:
-			title = soup.select_one('#contents_area_full > div.view2_summary.st3 > h3')
-			recipe_title = title.get_text()
-		except(AttributeError):
-			return
+			try:
+				title = soup.select_one('#contents_area_full > div.view2_summary.st3 > h3')
+				recipe_title = title.get_text()
+			except(AttributeError):
+				return
+	
+			try:
+				num = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info1')
+				recipe_num = num.get_text()
+			except(AttributeError):
+				print("num")
 
-		try:
-			num = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info1')
-			recipe_num = num.get_text()
-		except(AttributeError):
-			print("it is not have num")
+			try:
+				time = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info2')
+				recipe_time = time.get_text()
 
-		try:
-			time = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info2')
-			recipe_time = time.get_text()
-
-		except(AttributeError):
-			print("it is not have time")
-
+			except(AttributeError):
+				print("time")
 		#재료
-		try:
-			ingredient_tags = soup.select('#divConfirmedMaterialArea > ul > li')  # 재료 목록 선택
+			try:
+				ingredient_tags = soup.select('#divConfirmedMaterialArea > ul > li')  # 재료 목록 선택
 
-			for ingredient in ingredient_tags:
-				name_tag = ingredient.find('a')  # <a> 태그에서 재료 이름 추출
-				if name_tag:
-					recipe_source.append(name_tag.get_text().replace('\n','').replace(' ',''))
+				for ingredient in ingredient_tags:
+					name_tag = ingredient.find('a')  # <a> 태그에서 재료 이름 추출
+					if name_tag:
+						recipe_source.append(name_tag.get_text().replace('\n','').replace(' ',''))
 			
 		
-		except(AttributeError):
-			return	
+			except(AttributeError):
+				return	
 		
-		# 조리 순서
-		try:
-			step_divs = soup.select('#obx_recipe_step_start > div[id^="stepDiv"]')  # 조리 단계 선택
+			# 조리 순서
+			try:
+				step_divs = soup.select('#obx_recipe_step_start > div[id^="stepDiv"]')  # 조리 단계 선택
 
-			for step in step_divs:
-				for p in step.find_all('p'):
-					p.extract()
+				for step in step_divs:
+					for p in step.find_all('p'):
+						p.extract()
 
-				recipe_step.append(step.get_text(strip=True))
+					recipe_step.append(step.get_text(strip=True))
 	
 
-		except(AttributeError):
-			return
+			except(AttributeError):
+				return
 		
-	else:
-		print(page.status_code)
+		else:
+			print(page.status_code)
+
+	except requests.exceptions.RequestException as e:
+		print(f"Error: {e}")
 	
 	
 	recipe_all = [recipe_title, recipe_source, recipe_step, recipe_num, recipe_time]
@@ -101,29 +113,47 @@ def GetData(size):
 	recipes = []
 
 	for i in range(1,size+1):
+
+		if i % 100 == 0:
+			print(f'current page is {i}')
+
 		url = f'https://www.10000recipe.com/recipe/list.html?order=reco&page={i}'  
 
-		page = requests.get(url) 
+
+		session = requests.Session()
+		retry = Retry(total=5,  # 최대 재시도 횟수
+					backoff_factor=1,  # 재시도 간 대기 시간 증가
+					status_forcelist=[500, 502, 503, 504])  # 재시도할 상태 코드
+		adapter = HTTPAdapter(max_retries=retry)
+		session.mount('http://', adapter)
+		session.mount('https://', adapter)
+
+		try:
+
+			page = requests.get(url) 
 		
-		if page.status_code == 200:
-			soup = BeautifulSoup(page.content, 'html.parser')
+			if page.status_code == 200:
+				soup = BeautifulSoup(page.content, 'html.parser')
 
-			try:
-				step_divs = soup.select('#contents_area_full > ul > ul > li')
+				try:
+					step_divs = soup.select('#contents_area_full > ul > ul > li')
 				
-				for step in step_divs:
-					link = step.find('div',{'class':'common_sp_thumb'}).find('a')['href']
-					re = PageCrawler(link)
+					for step in step_divs:
+						link = step.find('div',{'class':'common_sp_thumb'}).find('a')['href']
+						re = PageCrawler(link)
 					
-					if re is not None:
-						recipes.append(re)
+						if re is not None:
+							recipes.append(re)
 
 
-			except(AttributeError):
-				print("error")
+				except(AttributeError):
+					print("error")
 
-		else:
-			break
+			else:
+				break
+		
+		except requests.exceptions.RequestException as e:
+			print(f"Error: {e}")
 	
 
 	SaveData(recipes)
@@ -131,7 +161,7 @@ def GetData(size):
 
 page = 100000
 
-GetData(2)
+GetData(page)
 
 
 
