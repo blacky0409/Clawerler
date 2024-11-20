b402f7dec0b363142f23dd439bcea7995745311d
blacky0409 <soccer1356@ajou.ac.kr>
2024-11-20 16:47:25 +0900

    Second push 1. if have blank entry. they dont save 2. add entry category

--- 10000_recipes/mining.py
+++ 10000_recipes/mining.py
@@ -5,10 +5,11 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import csv
 import pandas as pd
+import os
 
 baseUrl = 'https://www.10000recipe.com'
 
-def PageCrawler(Url):
+def PageCrawler(Url,name):
 	url = baseUrl + Url
 
 	session = requests.Session()
@@ -43,14 +44,14 @@ def PageCrawler(Url):
 				num = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info1')
 				recipe_num = num.get_text()
 			except(AttributeError):
-				print("num")
+				return
 
 			try:
 				time = soup.select_one('#contents_area_full > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info2')
 				recipe_time = time.get_text()
 
 			except(AttributeError):
-				print("time")
+				return
 		#재료
 			try:
 				ingredient_tags = soup.select('#divConfirmedMaterialArea > ul > li')  # 재료 목록 선택
@@ -85,7 +86,7 @@ def PageCrawler(Url):
 		print(f"Error: {e}")
 	
 	
-	recipe_all = [recipe_title, recipe_source, recipe_step, recipe_num, recipe_time]
+	recipe_all = [recipe_title, recipe_source, recipe_step, name,recipe_num, recipe_time]
 	return (recipe_all)
 
 def SaveData(rlist):
@@ -93,73 +94,83 @@ def SaveData(rlist):
 		'title': [],
 		'source': [],
 		'step': [],
+		'category': [],
 		'num': [],
 		'time': []
 	}
 
 	for recipe in rlist:
-		title, source, step, num, time = recipe
+		title, source, step,name, num, time = recipe
 		data['title'].append(title if title is not None else '')
 		data['source'].append(', '.join(source) if source is not None else '')  # 리스트를 문자열로 변환
 		data['step'].append('; '.join(step) if step is not None else '')  # 리스트를 문자열로 변환
+		data['category'].append(name if name is not None else '')
 		data['num'].append(num if num is not None else 0)  # 기본값 0
 		data['time'].append(time if time is not None else '')  # 빈 문자열
 
 	df = pd.DataFrame(data)
 
-	df.to_csv('recipes.csv', index=False, encoding='utf-8-sig')
+	file_path = 'recipes.csv'
+
+	if os.path.exists(file_path):
+		os.remove(file_path)
+	df.to_csv(file_path, index=False, encoding='utf-8-sig')
 
 def GetData(size):
 	recipes = []
+	page_link =[70,71,72,23,28,24,50,33,47,32,25,31,48,27,26,34]
+	page_link_name=["소고기","돼지고기","닭고기","육류","채소류","해물류","달걀/유제품","가공식품류","쌀","밀가루","건어물류","버섯류","과일류","콩/견과류","곡류","기타"]
 
-	for i in range(1,size+1):
+	for w in range(0,16):
+		for i in range(1,size+1):
 
-		if i % 100 == 0:
-			print(f'current page is {i}')
+			if i % 100 == 0:
+				print(f'current page is {i}')
 
-		url = f'https://www.10000recipe.com/recipe/list.html?order=reco&page={i}'  
+			linking = page_link[w]
+			url = f'https://www.10000recipe.com/recipe/list.html?cat3={linking}&order=reco&page={i}'  
 
 
-		session = requests.Session()
-		retry = Retry(total=5,  # 최대 재시도 횟수
+			session = requests.Session()
+			retry = Retry(total=5,  # 최대 재시도 횟수
 					backoff_factor=1,  # 재시도 간 대기 시간 증가
 					status_forcelist=[500, 502, 503, 504])  # 재시도할 상태 코드
-		adapter = HTTPAdapter(max_retries=retry)
-		session.mount('http://', adapter)
-		session.mount('https://', adapter)
+			adapter = HTTPAdapter(max_retries=retry)
+			session.mount('http://', adapter)
+			session.mount('https://', adapter)
 
-		try:
+			try:
 
-			page = requests.get(url) 
+				page = requests.get(url) 
 		
-			if page.status_code == 200:
-				soup = BeautifulSoup(page.content, 'html.parser')
+				if page.status_code == 200:
+					soup = BeautifulSoup(page.content, 'html.parser')
 
-				try:
-					step_divs = soup.select('#contents_area_full > ul > ul > li')
+					try:
+						step_divs = soup.select('#contents_area_full > ul > ul > li')
 				
-					for step in step_divs:
-						link = step.find('div',{'class':'common_sp_thumb'}).find('a')['href']
-						re = PageCrawler(link)
+						for step in step_divs:
+							link = step.find('div',{'class':'common_sp_thumb'}).find('a')['href']
+							re = PageCrawler(link,page_link_name[w])
 					
-						if re is not None:
-							recipes.append(re)
+							if re is not None:
+								recipes.append(re)
 
 
-				except(AttributeError):
-					print("error")
+					except(AttributeError):
+						print("error")
 
-			else:
-				break
+				else:
+					break
 		
-		except requests.exceptions.RequestException as e:
-			print(f"Error: {e}")
+			except requests.exceptions.RequestException as e:
+				print(f"Error: {e}")
 	
 
 	SaveData(recipes)
 
 
-page = 100000
+page = 1000000
 
 GetData(page)
 
